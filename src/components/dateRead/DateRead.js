98d54a777fcebae38d9df686c50f13cb75e012b7
Gabriel Golzman <ggolzman@primerosystems.com>
2023-05-02 18:58:42 -0300

    added book filter

--- src/components/dateRead/DateRead.js
+++ src/components/dateRead/DateRead.js
@@ -1,9 +1,11 @@
+import "./DateRead.css"
+
 const DateRead = ({ dateRead }) => {
   const year = dateRead.getFullYear();
   const month = dateRead.toLocaleString("es-AR", { month: "long" });
   const day = dateRead.toLocaleString("es-AR", { day: "2-digit" });
   return (
-    <div>
+    <div className="date-container">
       <div>{year}</div>
       <div>{month}</div>
       <div>{day}</div>
